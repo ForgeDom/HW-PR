09ff82f6c0d7d9208de1222f3fd4f5b6e4c0c340
ForgeDom <maxstill2006@gmail.com>
2024-04-18 20:39:43 +0300

    Ex 7 from class work is done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "Hello,World!" << endl;
+	cout << "To be, ";
+	SetConsoleTextAttribute(hConsole, 0x0D);
+	cout << "or not ";
 	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
+	cout << "to be: that is the question:" << endl << "Wheather ";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "'tis nobler in the mind to suffer" << endl << "The slings and arrows of outrageous fortune," << endl << "Or to take arms against a sea of troubles," << endl << "And by opposing end them? " << endl << "William Shakespeare";
 }
