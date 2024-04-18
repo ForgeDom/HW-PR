4b75b7c540d56e1faf1b6dd652eb1541607f354d
ForgeDom <maxstill2006@gmail.com>
2024-04-18 21:02:17 +0300

    Ex 2 from hometask is done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "Hello,World!" << endl;
+	SetConsoleTextAttribute(hConsole, 0x01);
+	cout << "Every" << endl;
+	SetConsoleTextAttribute(hConsole, 0x02);
+	cout << "\thunter" << endl;
+	SetConsoleTextAttribute(hConsole, 0x03);
+	cout << "\t\twants " << endl;
+	SetConsoleTextAttribute(hConsole, 0x04);
+	cout << "\t\t\tto " << endl;
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cout << "\t\t\t\tknow" << endl;
+	SetConsoleTextAttribute(hConsole, 0x06);
+	cout << "\t\t\t\t\twhere " << endl;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "\t\t\t\t\t\tthe" << endl;
+	SetConsoleTextAttribute(hConsole, 0x08);
+	cout << "\t\t\t\t\t\t\tpheasant " << endl;
+	SetConsoleTextAttribute(hConsole, 0x09);
+	cout << "\t\t\t\t\t\t\t\tis." << endl;
 	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
+	return 0;
 }
