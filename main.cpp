de73a7f5b2aa81f8cae509c967cccc6dcda60d98
ForgeDom <maxstill2006@gmail.com>
2024-04-18 20:24:24 +0300

    Ex 5 from class work is done

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "Hello,World!" << endl;
+	cout << "\"Hello,World!\"" << endl;
 	SetConsoleTextAttribute(hConsole, 0x0F);
 	cout << "/Bjarne Stroustrup/" << endl;
+	return 0;
 }
