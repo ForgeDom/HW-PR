87a2dfc2ecaf1f1f077af7cc1c9c721e204b1b5c
ForgeDom <maxstill2006@gmail.com>
2024-04-18 21:10:32 +0300

    Ex 3 from hometask is done

--- main.cpp
+++ main.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 
 int main() {
+	SetConsoleOutputCP(1251);
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << "Продам цуценят." << endl << "Порода: ротвейлер." << endl << "……" << endl << "Тел.: 222-22-22" << endl;
+	cout << "222      |   ";
 	SetConsoleTextAttribute(hConsole, 0x0E);
-	cout << "Hello,World!" << endl;
+	cout << "222   ";
 	SetConsoleTextAttribute(hConsole, 0x0F);
-	cout << "/Bjarne Stroustrup/" << endl;
+	cout << "|" << endl << "22       |   ";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "22    ";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "|" << endl << "22       |   ";
+	SetConsoleTextAttribute(hConsole, 0x0E);
+	cout << "22    ";
+	SetConsoleTextAttribute(hConsole, 0x0F);
+	cout << "|" << endl << "і т.д." << endl;
+	return 0;
 }
